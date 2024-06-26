b2a4bd5c85546f4e50b549dd71da501a43241e0b
georgepata <geo.patza@yahoo.com>
2024-06-26 15:42:44 +0300

    Modified User Model

--- SpendWise-Server.Models/User.cs
+++ SpendWise-Server.Models/User.cs
@@ -15,6 +15,12 @@ namespace SpendWise_Server.Models
         [MaxLength(256)]
         [Required]
         public string? Email { get; set; }
-        public DateTime? CreatedDate { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        public ICollection<Economies> Economies { get; set; } = [];
+        public ICollection<Incomes> Incomes {get; set;} = [];     
+        public ICollection<Income_Categories> Income_Categories {get; set;} = [];
+        [Required]
+        public Currencies? Currency {get; set;}
     }
 }
