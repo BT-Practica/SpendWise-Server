bd2cc72fc4de4232407fe1445c538d3315b90ab3
constantinrazvan <76180811+constantinrazvan@users.noreply.github.com>
2024-07-01 01:58:49 +0300

    economoy controller created

--- SpendWise-Server.Business/Interfaces/IEconomyService.cs
+++ SpendWise-Server.Business/Interfaces/IEconomyService.cs
@@ -1,13 +1,14 @@
 ﻿using SpendWise_Server.Models;
+using SpendWise_Server.Models.DTOs.EconomyDtos;
 
 namespace SpendWise_Server.Business.Interfaces
 {
     public interface IEconomyService
     {
         List<Economies> GetAllEconomies();
-        Economies GetEconomy(string economyId);
-        Task CreateEconomy(Economies economy);
-        Task UpdateEcnomy(Economies economy, int id);
+        Economies GetEconomy(int economyId);
+        Task CreateEconomy(EconomyDto economy);
+        Task UpdateEcnomy(EconomyDto economy, int id);
         Task DeleteEconomy(int id);
     }
 }
